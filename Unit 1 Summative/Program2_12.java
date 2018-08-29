37f805119fc93df8d16d05b9217fc4a2cf639c56
AngelaLuo49021 <42478474+AngelaLuo49021@users.noreply.github.com>
2018-08-29 14:21:30 -0500

    did currency total program

--- Unit 1 Summative/Program2_12.java
+++ Unit 1 Summative/Program2_12.java
@@ -2,12 +2,38 @@
 
 //Angela Luo
 //8/27/2018
+import java.util.*;
+import java.text.NumberFormat;
 public class Program2_12
 {
    public static void main (String[] args){
+       //creates scanner input from keyboard
+       Scanner keyboard = new Scanner (System.in);
        
+       //asks users for different coins
+       System.out.println("Enter # of pennies: ");
+       double p = keyboard.nextDouble();
+       System.out.println("Enter # of nickels: ");
+       double n = keyboard.nextDouble();
+       System.out.println("Enter # of dimes: ");
+       double d= keyboard.nextDouble();
+       System.out.println("Enter # of quarters: ");
+       double q = keyboard.nextDouble();
        
+       //calculates value of coins
        
+       double vp = p*.01;
+       double vn = n*.05;
+       double vd = d*.1;
+       double vq = q*.25;
+       double total = vp+vn+vd+vq;
+       
+       //creates currency formatting
+       NumberFormat fmt = NumberFormat.getCurrencyInstance();
+       
+       //prints out results
+       System.out.println("The total value of your coins is :"+fmt.format(total));
+      
        
        
        
